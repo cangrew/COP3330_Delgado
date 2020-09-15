4faac16f20514aac1334db81af5d4aa9d72d5f35
Andres Delgado <andresddelgado@knights.ucf.edu>
2020-09-14 22:29:23 -0400

    UI completed in Application.

--- Delgado_p1/src/Application.java
+++ Delgado_p1/src/Application.java
@@ -5,15 +5,39 @@ import java.awt.*;
 
 public class Application {
 	public static void main(String[] args) {
-    String input = "";
-    while(!input.equals("9999")){
-      Scanner kb = new Scanner(System.in);
-      input = kb.nextLine();
-  		Encrypter myEncrypter = new Encrypter();
-      Decrypter myDecrypter = new Decrypter();
-      String str = myEncrypter.encrypt(input);
-      str = myDecrypter.decrypt(str);
-      System.out.println(str);
-    }
+    run();
 	}
+
+  public static void run(){
+    Scanner kb = new Scanner(System.in);
+    System.out.print("\n\t1: Encrypt\n\t2: Decrypt\n\t3: Exit\n\nChoose mode: ");
+    int op = kb.nextInt();
+    String input;
+
+    //This works to clean the input stream.
+    kb.nextLine();
+
+    if(op == 1){
+      Encrypter myE = new Encrypter();
+      System.out.println("\nEncryption mode");
+      System.out.print("\nEnter value to encrypt: ");
+      input = kb.nextLine();
+      String encrypted = myE.encrypt(input);
+      System.out.println("Encrypted Value is: "+encrypted);
+    }
+    else if(op == 2){
+      Decrypter myD = new Decrypter();
+      System.out.println("\nDecryption mode");
+      System.out.print("\nEnter value to decrypt: ");
+      input = kb.nextLine();
+      String decrypted = myD.decrypt(input);
+      System.out.println("Decrypted Value is: "+decrypted);
+    }
+    else{
+      return;
+    }
+
+    run();
+  }
+
 }
