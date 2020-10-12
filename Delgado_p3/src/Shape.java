767afbe7c70c9eba83aa797a86a0911288eb23ff
Andres Delgado <andresddelgado@knights.ucf.edu>
2020-10-11 20:10:33 -0400

    Shape, Shape2D and Shape3D files created.

---
+++ Delgado_p3/src/Shape.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+
+abstract class Shape {
+
+  
+	public static void main(String[] args) {
+
+	}
+}
---
+++ Delgado_p3/src/Shape2D.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+
+abstract class Shape2D extends Shape {
+	public static void main(String[] args) {
+		/* code */
+	}
+}
